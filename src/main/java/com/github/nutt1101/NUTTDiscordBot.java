df1551a7e5d4cdccf2b817a7c412b2728ef249ab
cocoNUTTree <69194166+NUTT1101@users.noreply.github.com>
2021-09-10 11:46:04 +0800

    Add files via upload

---
+++ src/main/java/com/github/nutt1101/NUTTDiscordBot.java
@@ -0,0 +1,18 @@
+package com.github.nutt1101;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NUTTDiscordBot extends JavaPlugin{
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    
+}
