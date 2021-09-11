4134a3f3250790889035a28349a10a36b9cdfb51
NUTT1101 <zhangf099@gmail.com>
2021-09-11 15:29:26 +0800

    v1.0

---
+++ src/main/java/com/github/nutt1101/SSLHelper.java
@@ -0,0 +1,48 @@
+package com.github.nutt1101;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+
+/**
+ * code from <a href="https://stackoverflow.com/questions/40742380/how-to-resolve-jsoup-error-unable-to-find-valid-certification-path-to-requested/40742828#answer-58325184">stackoverflow.com</a>
+ */
+public class SSLHelper {
+
+    static public Connection getConnection(String url){
+        return Jsoup.connect(url).sslSocketFactory(SSLHelper.socketFactory());
+    }
+
+    static private SSLSocketFactory socketFactory() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            SSLSocketFactory result = sslContext.getSocketFactory();
+
+            return result;
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Failed to create a SSL socket factory", e);
+        }
+    }
+}
--- src/main/java/com/github/nutt1101/discord_listener/BotCommand.java
+++ src/main/java/com/github/nutt1101/discord_listener/BotCommand.java
@@ -3,6 +3,11 @@ package com.github.nutt1101.discord_listener;
 import java.awt.Color;
 
 import com.github.nutt1101.Config;
+import com.github.nutt1101.SSLHelper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import org.jsoup.nodes.Document;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -20,6 +25,7 @@ public class BotCommand extends ListenerAdapter{
         if (event.getMessage().getContentRaw().equalsIgnoreCase("dayeh")) {
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setAuthor("點我進入大葉大學官網", Config.bootEmAuthorTextLink, Config.bootEmAuthorImageLink);
+            embedBuilder.setDescription("[手機版請點我](http://fresh.dyu.edu.tw/)");
             embedBuilder.setColor(Color.decode(Config.bootEmHexColor));
             event.getChannel().sendMessage(embedBuilder.build()).queue();
         }
@@ -27,8 +33,37 @@ public class BotCommand extends ListenerAdapter{
         if (event.getMessage().getContentRaw().equalsIgnoreCase("fresh")) {
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setAuthor("點我進入大葉大學新生專區", "http://fresh.dyu.edu.tw/", Config.bootEmAuthorImageLink);
+            embedBuilder.setDescription("[手機版請點我](http://fresh.dyu.edu.tw/)");
             embedBuilder.setColor(Color.decode(Config.bootEmHexColor));
             event.getChannel().sendMessage(embedBuilder.build()).queue();;
+        }
+
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("fresh_ann")) {
+
+            try {
+                Document document = SSLHelper.getConnection("https://bulletin.dyu.edu.tw/index.php?goBack=1&isHidden=1&pool_ID=37").get();
+                String data = document.getElementsByTag("script").get(5).toString().split("\n")[5].split("'")[1];
+                JsonParser parser = new JsonParser();
+                JsonArray jsonArray = parser.parse(data).getAsJsonArray();
+
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                String description = "";
+
+                for (int i=0; i< 15; i++) {
+                    embedBuilder.setAuthor(document.title() + "公告", "http://fresh.dyu.edu.tw/", Config.bootEmAuthorImageLink);
+
+                    description = description + String.valueOf(i + 1) +  ". [" + jsonArray.get(i).getAsJsonObject().get("title").getAsString() + "]" + "(" + 
+                        "https://www.dyu.edu.tw/news.html?msg_ID=" + jsonArray.get(i).getAsJsonObject().get("ID").getAsString() +"&pool_ID=37&isHidden=1&goBack=1"
+                        +")\n\n";
+                    
+                }
+                embedBuilder.setDescription(description);
+                event.getChannel().sendMessage(embedBuilder.build()).queue();;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }               
+            
         }
     }
     
